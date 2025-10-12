427cf65c859177634130d374fc8487525f6843b2
Matheus Araujo <araujooomatheus8@gmail.com>
2025-10-11 23:28:15 -0300

    Criado a conexão com BD

---
+++ codigoPWEB/conexao.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost"; // servidor
+$usuario = "root";   // usuário padrão do XAMPP
+$senha = "";         // senha padrão é vazia
+$banco = "heliomax"; // nome do seu banco de dados
+
+// Criar conexão
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+} else {
+    // echo "Conectado com sucesso!";
+}
+?>
