321a1d6a9dbbbb4c4dbb0e7ed2651e7e235845e1
Felipe Antonio Parolin <felipeparolin10@gmail.com>
2025-10-15 20:17:44 -0300

    mudancaNome

--- codigoPWEB/pages/PHP/dashADM.php
+++ codigoPWEB/pages/PHP/dashADM.php
@@ -434,7 +434,7 @@ $usuario = $stmt->fetch();
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale-1.0" />
   <link rel="icon" type="image/png" href="../../images/icon.png">
-  <title>HelioMax Admin Dashboard</title>
+  <title>Dashboard do Administrador</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://unpkg.com/lucide@latest"></script>
   <style>
--- codigoPWEB/pages/PHP/dashUSER.php
+++ codigoPWEB/pages/PHP/dashUSER.php
@@ -94,7 +94,7 @@ $historico_recargas = [
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/png" href="../../images/icon.png">
-    <title>HelioMax - Painel do Usuário</title>
+    <title>Dashboard do Usuário</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/lucide@latest"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
